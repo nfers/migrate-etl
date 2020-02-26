5db5fe35305f1a8cde7cf2801c2b21219b64cb8a
Nayara Ferreira <nayara.ferreira@tron.com.br>
2020-02-26 16:03:46 -0300

    inclusao de constantes de configuracao das bases de dados MySQL e MongoDB

---
+++ config.py
@@ -0,0 +1,11 @@
+
+# config MySQL
+MYSQL_USERNAME = "root"
+MYSQL_PASSWORD = "root"
+MYSQL_HOST = "localhost"
+MYSQL_DB = "servpred"
+
+# config MongoDB 
+MONGO_HOST = "localhost"
+MONGO_PORT = 27017
+MONGO_DB = "servpred"
